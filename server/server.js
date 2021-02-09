a87b9b7d1194ecedf7d91ef02953bc84b395de55
Johnny Smiles <70188711+Johnny-smiles@users.noreply.github.com>
2021-02-09 12:23:45 -0600

    added server

--- server/server.js
+++ server/server.js
@@ -1,21 +1,48 @@
+// linking express
 const express = require('express');
+// linking apollo
+const { ApolloServer } = require('apollo-server-express');
+// linking path
 const path = require('path');
+
+// linking and destructoring schemas
+const { typeDefs, resolvers } = require('./schemas');
+// linking auth 
+const { authMiddleware } = require('./utils/auth');
+// linking connections js
 const db = require('./config/connection');
-const routes = require('./routes');
 
-const app = express();
+// establishing port
 const PORT = process.env.PORT || 3001;
+// establishing app
+const app = express();
+// initiaing apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
-app.use(express.urlencoded({ extended: true }));
+
+server.applyMiddleware({ app });
+
+// preventing rewrite
+app.use(express.urlencoded({ extended: false }));
+// extending app to json format
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+// Serve up static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
